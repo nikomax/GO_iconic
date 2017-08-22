1f754cb9c41ac3ff267102d8a8a71950750ac41c
nikomax <maks.chuprina@gmail.com>
2017-08-22 09:47:56 +0300

    Google map added

---
+++ js/common.js
@@ -0,0 +1,14 @@
+function initMap() {
+    var myLatLng = {lat: 50.451052, lng: 30.466432};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Hello World!'
+    });
+}
